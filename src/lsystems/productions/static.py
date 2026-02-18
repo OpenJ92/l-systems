393a9ed50d98cebf93cd24ead5e183b96156cf44
OpenJ92 <jacob.vartuli.92@gmail.com>
2026-02-17 21:40:08 -0500

    Static callable Production example

--- src/lsystems/productions/static.py
+++ src/lsystems/productions/static.py
@@ -1,2 +1,11 @@
-def static(sentence: Sentence):
-    return lambda symbol, scope: sentence
+from src.lsystems.protocols.sentence import Sentence
+from src.lsystems.protocols.production import Production
+from src.lsystems.generate import ScopeBundle
+from collections.abc import Hashable
+
+class Static(Production):
+    def __init__(self, sentence: Sentence):
+        self.sentence = sentence.clone()
+
+    def __call__(self, symbol: Hashable, scope: ScopeBundle):
+        return self.sentence
